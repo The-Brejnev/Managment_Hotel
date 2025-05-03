050d5401dbe2949ebfa84cacb6eeb9ab36a8bde2
The-Brejnev <bhertrandmatega@gmail.com>
2025-05-03 15:24:51 +0200

    Soft

--- frontend/components/layout/footers.jsx
+++ frontend/components/layout/footers.jsx
@@ -5,14 +5,14 @@ function Footers() {
     <footer className='footer'>
       <h2>Beach Resort | Hotel Room Booking System</h2>
       <p>
-        ©2023 Beach Resort — Developed By
+        ©2025 Beach Resort — Developed By
         {' '}
         <a
-          href='https://srmukul.com'
+          href='https://github.com/The-Brejnev'
           rel='noreferrer'
           target='_blank'
         >
-          Md. Samiur Rahman (Mukul)
+          Matega Brejnev (Brej)
         </a>
       </p>
     </footer>
